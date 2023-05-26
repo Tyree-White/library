9ec1ae5d3b273758c8d91c5e86ac3bfd3f1b7772
Tyree <tyreewhite01102006@gmail.com>
2023-05-26 15:03:44 -0500

    Add book to library

--- script.js
+++ script.js
@@ -8,3 +8,6 @@ function book(title, author, pages, read) {
 book.prototype.info = function() {
     return `${title} by ${author}, ${pages} pages, ${read}`;
 }
+
+const bookOne = book('The Hobbit', 'J.R.R. Tolkien, 295, read');
+console.log(bookOne);
