fd667054f09f03f13507bddcba67b8bb7015f8d9
Tyree <tyreewhite01102006@gmail.com>
2023-05-26 15:03:09 -0500

    Add object constructor & function for books

---
+++ script.js
@@ -0,0 +1,10 @@
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+book.prototype.info = function() {
+    return `${title} by ${author}, ${pages} pages, ${read}`;
+}
