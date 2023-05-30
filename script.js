f6340e652eafd1e1264d9b149be0d8d76924a348
Tyree <tyreewhite01102006@gmail.com>
2023-05-29 19:04:40 -0500

    Add form content to div

--- script.js
+++ script.js
@@ -63,7 +63,27 @@ function addBook() {
     const author = document.querySelector('#author').value;
     const pages = document.querySelector('#pages').value;
 
+    const bookDiv = document.createElement('div');
+        bookDiv.className = 'book';
+        container.appendChild(bookDiv);
 
+        const titleDiv = document.createElement('div');
+        titleDiv.className = 'title';
+        const authorDiv = document.createElement('div');
+        authorDiv.className = 'author';
+        const pagesDiv = document.createElement('div');
+        pagesDiv.className = 'pages';
+        const readDiv = document.createElement('div');
+        readDiv.className = 'read';
+
+        bookDiv.appendChild(titleDiv);
+        bookDiv.appendChild(authorDiv);
+        bookDiv.appendChild(pagesDiv);
+        bookDiv.appendChild(readDiv);
+
+        titleDiv.textContent = `${title}`;
+        authorDiv.textContent = `${author}`;
+        pagesDiv.textContent = `${pages}`;
     bookForm.style.display = 'none';
 }
 
