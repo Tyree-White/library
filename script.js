ec39f44e6c6ba0a6ec1ce0bd380ff5df8012f543
Tyree <tyreewhite01102006@gmail.com>
2023-06-05 22:13:37 -0500

    Delete unused code

--- script.js
+++ script.js
@@ -7,12 +7,6 @@ function book(title, author, pages, read) {
     this.read = read;
 }
 
-book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-}
-
-const bookOne = new book('The Hobbit', 'J.R.R. Tolkien', 295, 'Read');
-
 let myLibrary = [];
 
 function addBookToLibrary(book) {
